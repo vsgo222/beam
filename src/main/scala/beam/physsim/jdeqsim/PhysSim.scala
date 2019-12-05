31c073393a93857514cf8a808b08e95a4c6c6d71
Artavazd Balaian <reasyu@gmail.com>
2019-12-05 11:02:53 +0700

    Write events only on the last iteration

--- src/main/scala/beam/physsim/jdeqsim/PhysSim.scala
+++ src/main/scala/beam/physsim/jdeqsim/PhysSim.scala
@@ -196,7 +196,7 @@ class PhysSim(
       printStats(firstResult, lastResult)
       lastResult
     } else {
-      val simulationResult = simulate(currentIter, writeEvents = true)
+      val simulationResult = simulate(currentIter, shouldWritePhysSimEvents && currentIter == nIterations)
       carTravelTimeWriter.writeRow(
         Vector(
           currentIter,
