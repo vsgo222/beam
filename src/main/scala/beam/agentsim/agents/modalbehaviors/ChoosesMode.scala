fc747cc22848260337a58cccbc37d0c0f6f563fa
irishwithaxe <irishwithaxe@gmail.com>
2020-08-22 23:42:13 +0300

    experiment with default file version from develop

--- src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
+++ src/main/scala/beam/agentsim/agents/modalbehaviors/ChoosesMode.scala
@@ -330,10 +330,8 @@ trait ChoosesMode {
             withParking = willRequestDrivingRoute
           )
         case Some(WALK) =>
-          // FIXME: Our previous tour was WALK, but this should not stop us to use RideHail or try transit!
-          // This is important for the secondary activties because you can end-up walking very far (the problem was that whenever the person chooses WALK, he will never use any other mode anymore)
           responsePlaceholders = makeResponsePlaceholders(boundingBox, withRouting = true)
-          makeRequestWith(withTransit = true, Vector(bodyStreetVehicle), withParking = false)
+          makeRequestWith(withTransit = false, Vector(bodyStreetVehicle), withParking = false)
         case Some(WALK_TRANSIT) =>
           responsePlaceholders = makeResponsePlaceholders(boundingBox, withRouting = true)
           makeRequestWith(withTransit = true, Vector(bodyStreetVehicle), withParking = false)
@@ -965,10 +963,6 @@ trait ChoosesMode {
           .get("beam-attributes")
           .asInstanceOf[AttributesOfIndividual]
       val availableAlts = Some(filteredItinerariesForChoice.map(_.tripClassifier).mkString(":"))
-
-//      if (filteredItinerariesForChoice.size == 1 && filteredItinerariesForChoice.head.tripClassifier == WALK) {
-//        log.info(s"Person ${id}, filteredItinerariesForChoice: ${filteredItinerariesForChoice}")
-//      }
 
       modeChoiceCalculator(
         filteredItinerariesForChoice,
