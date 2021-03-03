30c4831a462383399af182e868f898e51a49b12d
Nate <nlant@byu.edu>
2021-03-03 09:40:10 -0700

    Add more comments

--- src/main/java/beam/analysis/tscore/WavHandlersRunner.java
+++ src/main/java/beam/analysis/tscore/WavHandlersRunner.java
@@ -39,9 +39,11 @@ public class WavHandlersRunner {
        // log.info("Number of trips of WC users: " + );
        // log.info("Average wait time for WAVs: " + + " minutes.");
        // log.info("Average wait time for WC users for WAV: " + );
+       // log.info("Average travel time for WC users on WAV: " +
 
        // log.info("Total number of ridehail vehicles: " +
        // log.info("Average wait time for ridehail vehilces: " +
+       // log.info("Averge travel time for ridehail users: " +  
 
        log.info("Total wait time: " + wrdc.getTotalWaitTimeForAllPeople());
        log.info("Total number of ride hail trips: " + wrdc.getNumberOfTrips());
