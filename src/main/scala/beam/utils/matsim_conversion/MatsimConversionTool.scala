a8fd1877bad1aa209bf7af82dfca5c5ecc9e5a1b
Nate <nlant@byu.edu>
2021-04-05 17:43:24 -0600

    Prepare the 1 scenario config
    This scenario has all wc and no other population.

--- src/main/scala/beam/utils/matsim_conversion/MatsimConversionTool.scala
+++ src/main/scala/beam/utils/matsim_conversion/MatsimConversionTool.scala
@@ -23,7 +23,7 @@ import org.supercsv.prefs.CsvPreference
 
 object MatsimConversionTool extends App {
 
-  val dummyGtfsPath = "test/input_new/small_mix/r5/SLC.zip"
+  val dummyGtfsPath = "test/input_new/small_wc/r5/SLC.zip"
 
   if (null != args && args.length > 0) {
     val beamConfigFilePath = args(0) //"test/input/beamville/beam.conf"
