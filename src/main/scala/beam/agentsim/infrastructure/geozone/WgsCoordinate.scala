e1012298f2f7f75f8c952a8fb962a31b5b1f5988
Hayden Atchley <atchley.sha@gmail.com>
2021-08-10 16:31:26 -0600

    Update WGS converter to correct CRS (matches input network.xml)

--- src/main/scala/beam/agentsim/infrastructure/geozone/WgsCoordinate.scala
+++ src/main/scala/beam/agentsim/infrastructure/geozone/WgsCoordinate.scala
@@ -21,7 +21,7 @@ object WgsCoordinate {
   }
 
   private val geoUtilsUtm: GeoUtils = new GeoUtils {
-    override def localCRS: String = "epsg:26910"
+    override def localCRS: String = "epsg:4326"
   }
 
   def fromUtm(longitude: Double, latitude: Double): WgsCoordinate = {
