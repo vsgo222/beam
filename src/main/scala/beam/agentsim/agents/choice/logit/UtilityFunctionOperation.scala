22259d76346c53683c6f5729e7e35a3b19211bae
Christopher Day <christophersday@gmail.com>
2021-08-17 11:02:58 -0600

    Add new utility function opterators so LCCM works
    It is very easy to add new utility function operators. What this does is allow a large variety of utility parameters to be used in the choice model. This will be important to the tour purpose model as well.

--- src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionOperation.scala
+++ src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionOperation.scala
@@ -18,10 +18,18 @@ object UtilityFunctionOperation {
 
   def apply(s: String, value: Double): UtilityFunctionOperation = {
     (s.toLowerCase, value) match {
-      case ("intercept", _)  => Intercept(value)
-      case ("asc", _)        => Intercept(value)
-      case ("multiplier", _) => Multiplier(value)
-      case _                 => throw new RuntimeException(s"Unknown Utility Parameter Type $s")
+      case ("intercept", _)      => Intercept(value)
+      case ("asc", _)            => Intercept(value)
+      case ("multiplier", _)     => Multiplier(value)
+      case ("householdsize", _)  => Multiplier(value) // for LCCM use only
+      case ("income", _)         => Multiplier(value) // for LCCM use only
+      case ("male", _)           => Multiplier(value) // for LCCM use only
+      case ("numbikes", _)       => Multiplier(value) // for LCCM use only
+      case ("numcars", _)        => Multiplier(value) // for LCCM use only
+      case ("surplus", _)        => Multiplier(value) // for LCCM use only
+      case ("cost", _)           => Multiplier(value)
+      case ("time", _)           => Multiplier(value)
+      case _                     => throw new RuntimeException(s"Unknown Utility Parameter Type $s")
     }
   }
 }
