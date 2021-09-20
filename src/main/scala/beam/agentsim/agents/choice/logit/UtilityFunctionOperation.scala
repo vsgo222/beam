50ffdb95b5dc17f91965bd0e2d25c48d4dcc7745
Christopher Day <christophersday@gmail.com>
2021-09-20 17:24:11 -0600

    Make vehicle travel time general for all modes; add waitTime variable

--- src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionOperation.scala
+++ src/main/scala/beam/agentsim/agents/choice/logit/UtilityFunctionOperation.scala
@@ -17,7 +17,7 @@ object UtilityFunctionOperation {
   }
 
   def apply(s: String, value: Double): UtilityFunctionOperation = {
-    (s.toLowerCase, value) match {
+    (s, value) match {
       case ("intercept", _)      => Intercept(value)
       case ("asc", _)            => Intercept(value)
       case ("multiplier", _)     => Multiplier(value)
@@ -27,13 +27,10 @@ object UtilityFunctionOperation {
       case ("numbikes", _)       => Multiplier(value) // for LCCM use
       case ("numcars", _)        => Multiplier(value) // for LCCM use
       case ("surplus", _)        => Multiplier(value) // for LCCM use
-      case ("cost", _)           => Multiplier(value) // for LCCM and TPCM use
-      case ("time", _)           => Multiplier(value) // for LCCM and TPCM use
-      case ("ivtt_car", _)       => Multiplier(value)
-      case ("ivtt_lightrail", _) => Multiplier(value)
-      case ("ivtt_ferry", _)     => Multiplier(value)
-      case ("ivtt_bus", _)       => Multiplier(value)
-      case ("ivtt_heavyrail", _) => Multiplier(value)
+      case ("cost", _)           => Multiplier(value) // for LCCM use
+      case ("time", _)           => Multiplier(value) // for LCCM use
+      case ("vehicleTime", _)    => Multiplier(value) // for TPCM use
+      case ("waitTime", _)       => Multiplier(value) // for TPCM use
       case _                     => throw new RuntimeException(s"Unknown Utility Parameter Type $s")
     }
   }
