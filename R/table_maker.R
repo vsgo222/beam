da419e74c8b82e9a10f2e3bf3540166cf0679fe6
Christopher Day <christophersday@gmail.com>
2021-10-01 17:03:14 -0600

    Update model analysis code to be more concise

--- R/table_maker.R
+++ R/table_maker.R
@@ -1,4 +1,95 @@
 
+# the following functions create the basic coefficient and constant tables of the models by reading in 
+# the input files iteratively or by making the table by scratch. it is put in this file so that they can
+# easily be read and created in other files. 
+
+read_utah_cf <- function(cf_files,cf_path){
+  for(i in 1:length(cf_files)) { 
+    assign(paste0("cf", i),
+           read_fwf(file = paste0(cf_path, cf_files[i]), skip = 1, fwf_widths(c(30,1,19,1,100))) %>% 
+             na_if("") %>% na.omit %>%
+             mutate(purpose = substr(cf_files[i],14,16),
+                    variable_type = "coefficient") %>%
+             rename(variable = X1, value = X3, description = X5) %>%
+             select(purpose, variable, variable_type, value, description)
+    )
+  } 
+  rbind(cf1,cf2,cf3,cf4,cf5)
+} 
+
+read_utah_cst <- function(cst_files,cst_path) {
+  for(i in 1:length(cst_files)) { 
+    assign(paste0("cst", i),
+           read_fwf(file = paste0(cst_path, cst_files[i]), skip = 0, fwf_widths(c(13,1,8))) %>% 
+             na_if("") %>% na.omit %>%
+             mutate(purpose = substr(cst_files[i],11,13),
+                    variable_type = "constant") %>%
+             rename(variable = X1, value = X3) %>%
+             select(purpose, variable, variable_type, value)
+    )
+  } 
+  rbind(cst1,cst2,cst3,cst4,cst5)
+}
+
+read_wfrc_cf = function(cf_files,cf_path){
+  for(i in 1:length(coeff_files)) { 
+    assign(paste0("coeff", i),
+           read_fwf(file = paste0(coeff_path, coeff_files[i]), skip = 1, fwf_widths(c(28,1,19,1,100))) %>% 
+             na_if("") %>% na.omit %>%
+             mutate(purpose = substr(coeff_files[i],1,3),
+                    variable_type = "coefficient") %>%
+             rename(variable = X1, value = X3, description = X5) %>%
+             select(purpose, variable, variable_type, value, description,-X2,-X4)
+    )
+  }
+  rbind(coeff1,coeff2,coeff3,coeff4)
+}
+
+read_wfrc_cst = function(cst_files,cst_path){
+  for(i in 1:length(const_files)) { 
+    assign(paste0("const", i),
+           read_fwf(file = paste0(const_path, const_files[i]), skip = 0, fwf_widths(c(19,1,14))) %>% 
+             na_if("") %>% na.omit %>%
+             mutate(purpose = paste0(substr(const_files[i],1,3),"_",substr(const_files[i],18,19)),
+                    variable_type = "constant") %>%
+             rename(variable = X1, value = X3) %>%
+             select(purpose, variable, variable_type, value)
+    )
+  }
+  rbind(const1,const2,const3,const4,const5,const6,const7)
+}
+
+build_nchrp_hbw = function(){
+  data.frame(
+    purpose = "HBW", 
+    variable = c("ivt_coef","ovt_coef","walk_coef","initwait_coef","xferwait_coef","cost_coef"), 
+    variable_type = "coefficient",
+    model_source = c("H","I","H","H","H","H"),
+    value = c(-0.033,-0.05,-0.093,-0.038,-0.038,-0.0021))
+}
+
+build_nchrp_hbo = function(){
+  data.frame(
+    purpose = "HBO",
+    variable = c("ivt_coef","ovt_coef","walk_coef","initwait_coef","xferwait_coef","cost_coef","autocost_coef","parkcost_coef","transitcost_coef"), 
+    variable_type = "coefficient",
+    model_source = c("I","I","J","J","J","D","I","I","I"),
+    value = c(-0.008,-0.025,-0.075,-0.050,-0.050,-0.033,-0.01,-0.025,-0.01))
+}
+
+build_nchrp_nhb = function(){
+  data.frame(
+    purpose = "NHB",
+    variable = c("ivt_coef","ovt_coef","walk_coef","initwait_coef","xferwait_coef","cost_coef","autocost_coef","parkcost_coef","transitcost_coef"), 
+    variable_type = "coefficient",
+    model_source = c("M","I","M","M","M","M","I","I","I"),
+    value = c(-0.013,-0.05,-0.032,-0.032,-0.050,-0.002,-0.006,-0.016,-0.006))
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+# The following table functions are used to build a table with matching variables across the models
+# Since each dataset is slightly different, a different function is used to create each table
 
 asim_table <- function(asim_tour_coeffs,purpose){
   asim1<- asim_tour_coeffs %>% select(Variable, {{purpose}}) %>% 
@@ -73,6 +164,9 @@ nchrp_table <- function(nchrp_coeff,purp,nchrp_ovt_long,divider){
     select(term,estimate,std.error)
 }
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+#this grapher is just a test I beleive
 wgrapher <- function(purp,asim,utah,wfrc,nchrp){
   list(
   "ActivitySim" = asim,
@@ -87,8 +181,8 @@ wgrapher <- function(purp,asim,utah,wfrc,nchrp){
   coord_flip() + ggpubr::rotate_x_text() 
 }
 
+# this grapher is used to compare the utility values of all the models on a negative log based x axis
 neg_log_trans = function() trans_new("neg_log", function(x) log(abs(x)), function(x) log(abs(x)))
-
 loggrapher <- function(purp,asim,utah,wfrc,nchrp){ 
   list(
     "ActivitySim" = asim,
@@ -104,11 +198,8 @@ loggrapher <- function(purp,asim,utah,wfrc,nchrp){
   theme_bw() +
   annotation_logticks(scaled = FALSE, alpha = .3,short = unit(15000,"mm"), colour = "gray")
 }
-#coord_flip() + ggpubr::rotate_x_text() +
-#scale_x_continuous(trans = weird, breaks = breaks_log(8)(c(0.1,1))) +
-#scale_x_log10(breaks = breaks_log(5)) +
 
-
+# this grapher is used to compare the utility/IVTT ratio values of all the models based on a log based x axis
 ivtt_ratio_grapher <- function(purp,asim,utah,wfrc,nchrp){
   list(
     "ActivitySim" = asim,
@@ -123,11 +214,3 @@ ivtt_ratio_grapher <- function(purp,asim,utah,wfrc,nchrp){
   scale_x_continuous(trans = "log10", labels = function(x) format(x, scientific = FALSE)) +
    theme_bw()
 }
-  
-
-
-weird <- scales::trans_new("neg_log2",
-                           transform=function(x) log(abs(x)),
-                           inverse=function(x) exp(abs(x))
-                           #breaks = breaks_log())
-)
