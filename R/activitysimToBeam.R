7e0fa4bc2b7b864e9db20f7429318e8f96d6289b
Hayden Atchley <atchley.sha@gmail.com>
2022-01-29 12:13:19 -0700

    Add mode fix to R script

--- R/activitysimToBeam.R
+++ R/activitysimToBeam.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(magrittr)
 
+#############################################################
 # set wd
 wd <- "test/input/csv_test/data"
 
@@ -9,9 +10,51 @@ persons <- read_csv(paste0(wd,"/final_persons.csv"))
 hh <- read_csv(paste0(wd,"/final_households.csv"))
 plans <- read_csv(paste0(wd,"/final_plans.csv"))
 
-#rewrite plans
+###############################################################
+
+####rewrite plans
+#fix person ids
 plans$person_id %<>% as.integer()
+
+#fix modes
+avail_modes <- c("bike", "walk", "car", "hov2", "hov2_teleportation",
+                 "hov3", "hov3_teleportation", "drive_transit",
+                 "walk_transit", "ride_hail", "ride_hail_pooled")
+
+for(i in 1:length(plans$trip_mode)){
+  if(!is.na(plans$trip_mode[i])){
+    if(plans$trip_mode[i] == "BIKE"){
+      plans$trip_mode[i] = "bike"
+    } else if(plans$trip_mode[i] == "WALK"){
+      plans$trip_mode[i] = "walk"
+    } else if(plans$trip_mode[i] %in% c("DRIVEALONEFREE","DRIVEALONEPAY")){
+      plans$trip_mode[i] = "car"
+    } else if(plans$trip_mode[i] %in% c("SHARED2FREE","SHARED2PAY")){
+      plans$trip_mode[i] = ifelse(runif(1) < 0.5, "hov2", "hov2_teleportation")
+    } else if(plans$trip_mode[i] %in% c("SHARED3FREE","SHARED3PAY")){
+      plans$trip_mode[i] = ifelse(runif(1) < 1/3, "hov3", "hov3_teleportation")
+    } else if(plans$trip_mode[i] %in% c("DRIVE_COM","DRIVE_EXP","DRIVE_LOC",
+                                        "DRIVE_LRF","DRIVE_HVY")){
+      plans$trip_mode[i] = "drive_transit"
+    } else if(plans$trip_mode[i] %in% c("WALK_COM","WALK_EXP","WALK_LOC",
+                                        "WALK_LRF","WALK_HVY")){
+      plans$trip_mode[i] = "walk_transit"
+    } else if(plans$trip_mode[i] %in% c("TNC_SINGLE","TAXI")){
+      plans$trip_mode[i] = "ride_hail"
+    } else if(plans$trip_mode[i] == "TNC_SHARED"){
+      plans$trip_mode[i] = "ride_hail_pooled"
+    } else if(plans$trip_mode[i] %in% avail_modes){
+      plans$trip_mode[i] = plans$trip_mode[i]
+    } else{
+      plans$trip_mode[i] =
+        "we messed up mode conversion (check activitysim to BEAM R script)"
+    }
+  }
+}
+
 write_csv(plans, paste0(wd,"/final_plans.csv"), na = "")
+
+###############################################################
 
 #remove x,y if they exist
 if("x" %in% colnames(hh)) hh %<>% select(-x)
@@ -32,6 +75,7 @@ hh %>%
   left_join(hh_home, by = "household_id") %>% 
   write_csv(paste0(wd,"/final_households.csv"), na = "0")
 
+###############################################################
 
 #create vehicles file
 nveh <- sum(hh$auto_ownership)
@@ -48,3 +92,10 @@ tibble(
   householdId = veh_hh
 ) %>% 
   write_csv(paste0(wd,"/vehicles.csv"))
+
+##############################################################
+
+#sanity check
+plans$trip_mode[plans$trip_mode %in% c("hov2","hov2_teleportation")] %>% table()
+plans$trip_mode[plans$trip_mode %in% c("hov3","hov3_teleportation")] %>% table()
+plans$trip_mode %>% unique()
