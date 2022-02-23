8f72a22653cf61356e0a8d2094a63736ec4e15fc
Christopher Day <christophersday@gmail.com>
2022-02-22 20:51:05 -0700

    Update subtour analysis scripts

--- R/mc_memos/subtour_functions.R
+++ R/mc_memos/subtour_functions.R
@@ -11,6 +11,9 @@ get_purpose_subtours <- function(asim_final_trips, purp){
   
   asim_subtable <- asim_final_trips %>%
     
+    # make all purposes lowercase
+    mutate(purpose = tolower(purpose)) %>%
+    
     # find duplicated purposes in the same taz
     group_by(person_id, tour_id) %>%
     mutate(act = paste0(purpose,destination)) %>%
@@ -36,8 +39,10 @@ get_purpose_subtours <- function(asim_final_trips, purp){
     arrange(person_id,tour_id,trip_id) %>%
   
     # delete "subtours" that are simply just two of the same purposes in a row
-    mutate(sub = ifelse(sub == "startsub" & lead(sub) == "endsub", "none", ifelse(sub== "endsub" & lag(sub) == "startsub", "none", sub))) %>%
-    filter(person_id %in% c(61,133,1284, 6738, 28819))
+    mutate(sub = ifelse(sub == "startsub" & lead(sub) == "endsub", "none", 
+                        ifelse(sub== "endsub" & lag(sub) == "startsub", "none", 
+                               sub))) 
+    #filter(person_id %in% c(61,133,1284, 6738, 28819))
   
   # fill in the gaps between the beginning and end of each subtour
   for(i in 1:length(asim_subtable$sub)){
@@ -54,10 +59,42 @@ get_purpose_subtours <- function(asim_final_trips, purp){
   # techincally the beginsub is the trip before the beginning of a subtour
   asim_subtable %>%
     select(-sub1, -sub2) %>%
-    mutate(purpy = ifelse(sub %in% c("endsub", "midsub"),
+    mutate(sub = ifelse(sub %in% c("endsub", "midsub"),
                         purp, "none"))
 
 }  
 
 
+#---------------------------------------------------------------------------#
+# This function is used to merge together all the separate tables and create 
+# a single column that houses information on if the trip is a subtour and if so,
+# what type of subtour it is. It creates a list of multiple subtour types if the
+# subtour is embedded in another subtour.
+merge_subtours <- function(work_subtours, univ_subtours, school_subtours, escort_subtours, 
+                           shopping_subtours, eatout_subtours, othmaint_subtours, 
+                           social_subtours, othdiscr_subtours, atwork_subtours) {
+  
+  # merge together all the separatre subtours for each purpose into one table
+  subtour_purps <- plyr::join_all(list(
+    work_subtours,
+    univ_subtours,
+    school_subtours,
+    escort_subtours,
+    shopping_subtours,
+    eatout_subtours,
+    othmaint_subtours,
+    social_subtours,
+    othdiscr_subtours,
+    atwork_subtours
+  ), by = 'trip_id', type = 'left') %>%
+    select(1:11,21,31,41,51,61,71,81,91,101) %>%
+    
+    # the single column schould be nested to account for embedded subtours
+    na_if("none") %>%  
+    nest(11:20) %>%
+    mutate(subtours = lapply(data, function(x) x[!is.na(x)])) %>%
+    select(-data) 
+  
+  subtour_purps
+}
 
